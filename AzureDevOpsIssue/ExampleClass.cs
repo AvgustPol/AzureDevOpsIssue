f6be04c8a7c63532dedacffdc01795fcf420bf26
Anton Vlasiuk <anton.vlasiuk@external.toyota-europe.com>
2019-08-01 10:57:32 +0200

    Add project files.

---
+++ AzureDevOpsIssue/ExampleClass.cs
@@ -0,0 +1,20 @@
+﻿namespace AzureDevOpsIssue
+{
+    public class ExampleClass
+    {
+        public void DummyMethod()
+        {
+            // this method will be used in the example
+        }
+
+        public void VeryImportantMethod()
+        {
+            // this method contains very important functionality implementation
+        }
+
+        public void ThisShouldBeDeleted()
+        {
+            // this method should be deleted
+        }
+    }
+}
